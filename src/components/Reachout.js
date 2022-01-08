8196c297d85b8716de4e4b6c57dc1335341c0d8f
swetshaw <swt.shw94@gmail.com>
2022-01-08 10:48:34 +0530

    Added `Contact Form` in the Reachout Section

--- src/components/Reachout.js
+++ src/components/Reachout.js
@@ -1,15 +1,52 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 const Reachout = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_pjc9fob',
+        'contact_form',
+        form.current,
+        'user_1ZgKmsnKSmgFtOD9CyAmj'
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          form.current.reset()
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
-    <div className=' h-96 p-4 mt-8 flex flex-col justify-center items-center'>
+    <div className=' h-96 p-4 mt-8 flex flex-col gap-8 justify-center items-center'>
       <h1 className='font-extrabold text-4xl text-white'>
-        Reach out to us if you are an {' '}
+        Reach out to us if you are an{' '}
         <span className='text-pinktext'>Artist</span>
       </h1>
-      <button className='rounded p-2 shadow-md  shadow-indigo-500/40 w-1/4 mt-12 text-white text-lg font-semibold bg-gradient-to-r from-bluecolor via-purple-500 to-pinktext'>
-        Contact Us
-      </button>
+
+      <form name='contact_form'
+        className='flex flex-col w-64 items-cemter gap-2.5'
+        ref={form}
+        onSubmit={sendEmail}
+      >
+        <input className='rounded-sm' type='text' placeholder='Name' name='user_name' />
+
+        <input className='rounded-sm'  type='email' placeholder='Email' name='user_email' />
+
+        <textarea className='rounded-sm'  placeholder='Enter your message' name='message' />
+        <button
+          type='submit'
+          className='rounded p-2 shadow-md shadow-indigo-500/40 mt-2 text-white w-1/2 self-center text-lg font-semibold bg-gradient-to-r from-bluecolor via-purple-500 to-pinktext'
+        >
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
